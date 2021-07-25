120f419aa34193592f4316a5c1e5fb76153b1186
gr.zhu <55681140@163.com>
2021-07-25 11:36:20 +0800

    删除无用方法

--- app/src/main/java/com/ilab/yougetmobiledl/utils/AppExt.kt
+++ app/src/main/java/com/ilab/yougetmobiledl/utils/AppExt.kt
@@ -1,13 +1,8 @@
 package com.ilab.yougetmobiledl.utils
 
-import android.app.Activity
-import android.content.ComponentName
-import android.content.Intent
-import android.provider.Settings
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.ftd.livepermissions.LivePermissions
 import com.ftd.livepermissions.PermissionResult
@@ -25,15 +20,11 @@ const val TV_EXIT = "再按一次退出程序"
 // 最近一次点击的控件ID
 private var mLastClickViewId = 0
 
-fun AppCompatActivity.showToast(msg: String, time: Int = Toast.LENGTH_LONG) {
-    Toast.makeText(this, msg, time).show()
-}
-
 fun Fragment.showToast(msg: String, time: Int = Toast.LENGTH_LONG) {
     Toast.makeText(requireContext(), msg, time).show()
 }
 
-fun AppCompatActivity.exit() {
+fun exit() {
     if (System.currentTimeMillis() - exitTime > 2000L) {
         ToastUtils.showLong(TV_EXIT)
         exitTime = System.currentTimeMillis()
@@ -110,31 +101,6 @@ private fun isFastDoubleClick(v: View, intervalMillis: Long, withOthers: Boolean
         mLastClickTime = time
         mLastClickViewId = viewId
         false
-    }
-}
-
-fun Activity.openDevSetting() {
-    try {
-        val intent = Intent(Settings.ACTION_APPLICATION_DEVELOPMENT_SETTINGS)
-        startActivity(intent)
-    } catch (e: Exception) {
-        try {
-            val componentName = ComponentName(
-                "com.android.settings",
-                "com.android.settings.DevelopmentSettings"
-            )
-            val intent = Intent()
-            intent.component = componentName
-            intent.action = "android.intent.action.View"
-            startActivity(intent)
-        } catch (e1: Exception) {
-            try {
-                // 部分小米手机采用这种方式跳转
-                val intent = Intent("com.android.settings.APPLICATION_DEVELOPMENT_SETTINGS")
-                startActivity(intent)
-            } catch (e2: Exception) {
-            }
-        }
     }
 }
 
